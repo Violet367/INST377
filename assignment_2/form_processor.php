9eee63bf3dcd1a31f8b68d2d2018f1cebb4340e6
violet367 <violet_gooding@yahoo.com>
2018-11-27 21:54:14 -0500

    update 9:54

--- assignment_2/form_processor.php
+++ assignment_2/form_processor.php
@@ -15,6 +15,14 @@
 <?php
 // The code to process recieved data from the form goes to here.
 
+$name = $_POST["teamName"];
+$startTime = $_POST["start"];
+$endTime = $_POST["end"];
+$grade = $_POST["grade"];
+ 
+echo $name . "<br>";
+echo "Overall Grade: " . $grade;
+
 ?>
 </body>
 </html>
