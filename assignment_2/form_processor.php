867fef64781fb01424cd619f38b007b4796ed1d1
violet367 <violet_gooding@yahoo.com>
2018-11-27 22:52:36 -0500

    update 10:52

--- assignment_2/form_processor.php
+++ assignment_2/form_processor.php
@@ -19,7 +19,25 @@ $name = $_POST["teamName"];
 $startTime = $_POST["start"];
 $endTime = $_POST["end"];
 $grade = $_POST["grade"];
- 
+
+$mem1 = $_POST["name1"];
+$mem1Grade = $_POST["grade1"]
+
+$mem2 = $_POST["name2"];
+$mem2Grade = $_POST["grade2"]
+
+$mem3 = $_POST["name3"];
+$mem3Grade = $_POST["grade3"]
+
+$mem4 = $_POST["name4"];
+$mem4Grade = $_POST["grade4"]
+
+$mem5 = $_POST["name5"];
+$mem5Grade = $_POST["grade5"]
+
+$mem6 = $_POST["name6"];
+$mem6Grade = $_POST["grade6"]
+
 echo $name . "<br>";
 echo "Overall Grade: " . $grade;
 
