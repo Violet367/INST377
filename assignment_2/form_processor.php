2d7271bb25bc7c6c29d3a4328d9ec4fe6b2da066
violet367 <violet_gooding@yahoo.com>
2018-11-29 02:01:46 -0500

    update

--- assignment_2/form_processor.php
+++ assignment_2/form_processor.php
@@ -21,6 +21,7 @@ $startTime->format('Y-m-d H:i:s');
 $endTime =new DateTime($_POST["end"]);
 $endTime->format('Y-m-d H:i:s');
 $grade = $_POST["grade"];
+$notes = $_POST["notes"];
 
 $mem1 = $_POST["name1"];
 $mem1Grade = $_POST["grade1"];
@@ -40,28 +41,28 @@ $mem5Grade = $_POST["grade5"];
 $mem6 = $_POST["name6"];
 $mem6Grade = $_POST["grade6"];
 
-echo $name . "<br>";
-echo "Overall Grade: " . $grade . "<br>";
+echo "<b>". $name . "</b><br>";
+echo "<b>Overall Grade:</b> " . $grade . "<br>";
 
 
 $datetime = date_diff($startTime, $endTime);
-echo $datetime->format('%i minute presentation');
+echo $datetime->format('<b>Presentation Time:</b> %i minute presentation');
+echo "<br>";
 
-function LetterGrade($value){
+function letterGrade($value, $grade){
 
-	$letterNum = array('A+' => 1, 'A'=> 2, 'A-'=>3, 'B+'=>4, 'B'=>5, 'B-'=>6,
-	'C+' =>7, 'C'=> 8, 'C-'=>9, 'D+'=>10, 'D'=>11, 'D-'=>12, 'F'=> 13);
+	$letterNum = array("A+" => 1, "A"=> 2, "A-" =>3, "B+" =>4, "B"=>5, "B-"=>6,
+	"C+" =>7, "C"=> 8, "C-"=>9, "D+"=>10, "D"=>11, "D-"=>12, "F"=> 13);
 
-	$numLetter = array(1 => 'A+', 2 => 'A', 3=>'A-', 4=>'B+', 5=>'B', 6=>'B-',
-	7 => 'C+', 'C'=> 8, 'C-'=>9, 'D+'=>10, 'D'=>11, 'D-'=>12, 'F'=> 13);
+	$numLetter = array(1 => "A+", 2 => "A", 3=>"A-", 4=>"B+", 5=>"B", 6=>"B-",
+	7 => "C+", 8 => "C", 9 => "C-", 10 =>"D+", 11=> "D", 12=> "D-", 13=> "F");
 
 	$num = $letterNum[$grade];
 
 	switch ($value) {
 
 		case 'Above and Beyond':
-			echo '1';
-			echo $grade;
+
 			return $grade;
 			break;
 
@@ -96,12 +97,37 @@ function LetterGrade($value){
 
 if(!empty($mem1)){
 
-  echo "<br>" . $mem1 . " receives an " . LetterGrade($mem1Grade) . "<br>";
+  echo "<br>" . $mem1 . " receives an " . letterGrade($mem1Grade,$grade) . "<br>";
 }
 
+if(!empty($mem2)){
 
+  echo "<br>" . $mem2 . " receives an " . letterGrade($mem2Grade,$grade) . "<br>";
+}
 
+if(!empty($mem3)){
 
+  echo "<br>" . $mem3 . " receives an " . letterGrade($mem3Grade,$grade) . "<br>";
+}
+
+if(!empty($mem4)){
+
+  echo "<br>" . $mem4 . " receives an " . letterGrade($mem4Grade,$grade) . "<br>";
+}
+
+if(!empty($mem5)){
+
+  echo "<br>" . $mem5 . " receives an " . letterGrade($mem5Grade,$grade) . "<br>";
+}
+
+if(!empty($mem6)){
+
+  echo "<br>" . $mem6 . " receives an " . letterGrade($mem6Grade,$grade) . "<br>";
+}
+
+if(!empty($notes)){
+	echo "<br>" . "<b>Notes</b>" . "<br>" . $notes;
+}
 ?>
 </body>
 </html>
